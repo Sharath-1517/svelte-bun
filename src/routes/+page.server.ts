3d0e01098fe10e67d8e447748429e04a3ee4f6ea
S Sharath Lingam <111525679+Sharath-1517@users.noreply.github.com>
2023-09-25 19:49:58 +0530

    first commit

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,22 @@
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+	try {
+		const params = await fetch('https://jsonplaceholder.typicode.com/photos');
+		const paramsData = await params.json();
+
+		if (paramsData) {
+			return {
+				data: {
+					...paramsData
+				}
+			};
+		} else {
+			return {
+				message: 'Did not fetch anything',
+				status: 400
+			};
+		}
+	} catch (error: any) {
+		throw new Error(error);
+	}
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {page} from '$app/stores';
+	let count = 0;
+    let {data} = $page;
+    
+</script>
+
+{#if data!=undefined || data!=null}
+    <ul class="flex flex-col gap-4 list-none m-0 p-0">
+        {#each data.data as datum}
+            <li class="flex flex-col gap-3">
+
+            </li>
+        {/each}
+    </ul>
+{/if}
